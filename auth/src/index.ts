352be46f43234acfa6e3877deacb024fba2e306e
Talles Hentges <talles.hentges@gmail.com>
2021-03-15 13:35:44 -0300

    Setup mongo db infra and connect via mongoose

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import 'express-async-errors'
 import { json } from 'body-parser'
+import mongoose from 'mongoose'
 
 import { currentUserRouter } from './routes/currentUser'
 import { signInRouter } from './routes/signIn'
@@ -24,6 +25,22 @@ app.all('*', async () => {
 
 app.use(errorHandler)
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!')
-})
+const startUp = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+
+    console.log('Connected to MongoDB')
+  } catch (err) {
+    console.error(err)
+  }
+
+  app.listen(3000, () => {  
+    console.log('Listening on port 3000!')
+  })
+}
+
+startUp()
