23cd26fb213b5775bb0924629036479af0154f04
Talles Hentges <talles.hentges@gmail.com>
2021-03-17 17:44:25 -0300

    Create client header and _app getInitialProps logic

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.css'
 
-const App = ({ Component, pageProps }) =>
-  <Component {...pageProps} />
+import buildClient from '../api/buildClient'
 
-export default App
+import Header from '../components/Header'
+
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component currentUser={currentUser} {...pageProps} />
+    </div>
+  )
+}
+
+AppComponent.getInitialProps = async ({ Component, ctx }) => {
+  const client = buildClient(ctx)
+  const url = '/api/users/currentuser'
+  const { data } = await client.get(url)
+
+  let pageProps = {}
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx)
+  }
+
+  return { pageProps, ...data }
+}
+
+export default AppComponent
