437c70fe6f27cfcea14fdce795bcf95d33b0ed18
Talles Hentges <talles.hentges@gmail.com>
2021-03-18 08:26:48 -0300

    Test cases for new ticket route

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,23 @@
+import request from 'supertest'
+ 
+import { app } from '../../app'
+
+it('has a route handler listening to /api/tickets for post requrests', async () => {
+  
+})
+
+it('can only be accessed if the user is signed in', async () => {
+  
+})
+
+it('returns an error if an invalid title is provided', async () => {
+  
+})
+
+it('returns an error if an invalid price is provided', async () => {
+  
+})
+
+it('creates a ticket if inputs are valid', async () => {
+  
+})
