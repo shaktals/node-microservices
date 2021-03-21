78545c816f2d8ee368b6ab69da2800938cd8b260
Talles Hentges <talles.hentges@gmail.com>
2021-03-21 11:00:50 -0300

    Create orders index route and tests

---
+++ orders/src/routes/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express'
+
+import { requireAuth } from '@shaktickets/common'
+
+import { Order } from '../models/Order'
+
+const router = express.Router()
+
+router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
+    const orders = await Order
+      .find({ userId: req.currentUser!.id })
+      .populate('ticket')
+
+    res.send(orders)
+  }
+)
+
+export { router as indexOrderRouter }
