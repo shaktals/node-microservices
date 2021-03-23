c0167d8a9074544df664557da10e72582fa6b38f
Talles Hentges <talles.hentges@gmail.com>
2021-03-23 20:10:03 -0300

    Update Order and Ticket models to fix concurrency issues

--- orders/src/models/Ticket.ts
+++ orders/src/models/Ticket.ts
@@ -1,22 +1,31 @@
 import mongoose from 'mongoose'
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current'
 
 import { Order, OrderStatus } from './Order'
 
 // Describe props required for a new user
 interface TicketAttrs {
+  id: string
   title: string
   price: number
+}
+
+interface FindTicketParms {
+  id: string
+  version: number
 }
 
 // Describe props that a User model has
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc
+  findByEvent(event: FindTicketParms): Promise<TicketDoc | null>
 }
 
 // Describe props that a User document has
 interface TicketDoc extends mongoose.Document {
   title: string
   price: number
+  version: number
   isReserved(): Promise<boolean>
 }
 
@@ -40,8 +49,18 @@ const ticketSchema = new mongoose.Schema({
   versionKey: false,
 })
 
-ticketSchema.statics.build = (attrs: TicketAttrs) => {
-  return new Ticket(attrs)
+ticketSchema.set('versionKey', 'version')
+ticketSchema.plugin(updateIfCurrentPlugin)
+
+ticketSchema.statics.build = ({ id, ...rest }: TicketAttrs) => {
+  return new Ticket({
+    _id: id,
+    ...rest,
+  })
+}
+
+ticketSchema.statics.findByEvent = ({ id, version }: FindTicketParms) => {
+  return Ticket.findOne({ _id: id, version: version - 1 })
 }
 
 ticketSchema.methods.isReserved = async function() {
