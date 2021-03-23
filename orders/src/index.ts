1d7fc5b791bba99884034b78b46b4d6e41dcff8b
Talles Hentges <talles.hentges@gmail.com>
2021-03-23 20:29:58 -0300

    Update order service app index file and package.json

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -2,6 +2,8 @@ import mongoose from 'mongoose'
 
 import { app } from './app'
 import { natsWrapper } from './natsWrapper'
+import { TicketCreatedListener } from './events/listeners/TicketCreatedListener'
+import { TicketUpdatedListener } from './events/listeners/TicketUpdatedListener'
 
 const startUp = async () => {
   if (!process.env.JWT_KEY) {
@@ -34,6 +36,9 @@ const startUp = async () => {
 
     process.on('SIGINT', () => natsWrapper.client.close())
     process.on('SIGTERM', () => natsWrapper.client.close())
+
+    new TicketCreatedListener(natsWrapper.client).listen()
+    new TicketUpdatedListener(natsWrapper.client).listen()
 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
